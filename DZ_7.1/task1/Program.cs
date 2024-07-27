bcb37e59792c48615b70166d3162eb3148982f51
Фёдор <tereshkin.2015@list.ru>
2024-07-27 22:35:59 +1100

    Add files via upload

---
+++ DZ_7.1/task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// Использовать рекурсию, не использовать циклы.
+
+using System;
+
+public class Program
+{
+    public static void PrintRange(int m, int n)
+    {
+        if (m > n)
+            return;
+
+        Console.WriteLine(m);
+        PrintRange(m + 1, n);
+    }
+
+    public static void Main()
+    {
+        int m = 5;
+        int n = 10;
+
+        PrintRange(m, n);
+    }
+}
---
+++ DZ_7.1/task3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца.
+// Использовать рекурсию, не использовать циклы.
+
+using System;
+
+public class Program
+{
+    public static void PrintArrayReverse(int[] arr, int index)
+    {
+        if (index >= 0)
+        {
+            Console.Write(arr[index] + " ");
+            PrintArrayReverse(arr, index - 1);
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] myArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        Console.WriteLine("Элементы массива в обратном порядке:");
+        PrintArrayReverse(myArray, myArray.Length - 1);
+    }
+}
